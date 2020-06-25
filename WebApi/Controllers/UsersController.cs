7aa055fd51eb181a78181064cc407f7b90619aa1
Александр Сомов <19somovalexandr99@bk.ru>
2020-06-25 06:24:58 +0500

    3 UserUpdate

--- WebApi/Controllers/UsersController.cs
+++ WebApi/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using AutoMapper;
 using Game.Domain;
@@ -64,5 +65,36 @@ namespace WebApi.Controllers
                 CreateUserNew.Id);
         }
 
+        [HttpPost]
+        [Produces("application/json", "application/xml")]
+        public IActionResult UpdateUser([FromRoute, Required] Guid userId, [FromBody] CreaterUser user)
+        {
+
+            if (user == null)
+                return BadRequest();
+
+            if (!user.Login.All(c => char.IsLetter(c) || char.IsDigit(c)))
+                ModelState.AddModelError(nameof(CreaterUser),
+                    "Логин должен быть из цифр или букв!");
+
+            if (!ModelState.IsValid)
+                return new UnprocessableEntityObjectResult(ModelState);
+
+            var userFromRepo = userRepository.FindById(userId);
+            if (userFromRepo == null)
+            {
+                var userEntity = mapper.Map<UserEntity>(new UserEntity(userId));
+                userRepository.UpdateOrInsert(userEntity, out bool isInserted);
+                
+                return CreatedAtRoute(
+                    nameof(GetUserById),
+                    new { userId = userEntity.Id },
+                    userEntity.Id);
+            }
+
+            mapper.Map(user, userFromRepo);
+            userRepository.Update(userFromRepo);
+            return NoContent();
+        }
     }
 }
